d92373ee11e41db62122cd81e3db49dc9ae3fc18
Shubham Dutta <shubham.dutta877@gmail.com>
2026-03-23 12:30:01 +0530

    added Day2 files

---
+++ Day2/Type.java
@@ -0,0 +1,46 @@
+public class Type {
+    public static void main(String[] args) {
+        
+        // implicit conversion
+        //(byte to int)
+        // byte b = 24;
+        // int i;
+        // i=b;
+        // System.out.println(i); // 24
+
+        //(char to int)
+        // char c = 'a';
+        // int l ;
+        // l = c;
+        // System.out.println(l);
+
+        //Explicit conversion
+        // int i = 300;
+        // byte b;
+         
+        // b = (byte)i;
+        // System.out.println(b); // 44
+
+        // truncating conversion
+        // float f = 16.35f;
+        // int i ;
+        // i = (int)f;
+        // System.out.println(i);
+
+        //boolean to any data type
+        // these conversion are not possible in java
+         
+        // boolean bool = false;
+        // int i;
+
+        // byte b = 50;
+        // b = (byte)(b * 2);
+
+        // System.out.println(b); // 100
+
+
+        
+        
+    }
+    
+}
---
+++ Day2/Type2.java
@@ -0,0 +1,22 @@
+public class Type2 {
+    public static void main(String[] args) {
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = .1234;
+
+        double result = (f * b) + (i / c) - (d * s);
+
+        // float * byte -----> float
+        // int / char ------> int
+        // double * short -----> double
+
+        // float + int = float - double -----> double
+        
+        System.out.println((f * b) + " + " + (i / c) + " + " + (d * s));
+        System.out.println("result = " +result);
+    }
+    
+}

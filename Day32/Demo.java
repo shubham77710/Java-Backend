b363d4a103ce02279b957c03963b7650f228ca23
Shubham Dutta <shubham.dutta877@gmail.com>
2026-05-02 23:48:32 +0530

    added Day32 files

---
+++ Day32/Demo.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+
+        list.add(new Student( "Aditya",95));
+        list.add(new Student("Rohit",85));
+        list.add(new Student("Rohan", 56));
+        list.add(new Student("Monu",85));
+
+        Collections.sort(list); // ascending order sort
+
+        for(Student s : list){
+            System.out.println(s.name + " "+ s.marks);
+        }
+        // System.out.println(list);
+
+        // List<Integer> list2 = new ArrayList<>();
+
+        // list2.add(10);
+        // list2.add(5);
+        // list2.add(45);
+
+        // Collections.sort(list2);
+
+        // System.out.println(list2);
+
+
+    }
+}
+
+class Student implements Comparable<Student>{
+    int marks;
+    String name;
+
+    Student(String name,int marks){
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public int compareTo(Student other){
+        // return this.marks - other.marks;
+        // return  other.marks- this.marks ;
+
+        if(this.marks != other.marks){
+            return this.marks - other.marks;
+        }
+
+        return this.name.compareTo(other.name);
+
+
+        
+
+    }
+}
+
+// this.marks - other.marks;
+// < 0 : this.marks, other.marks
+// > 0 :other.marks ,  this.marks ;
---
+++ Day32/Demo2.java
@@ -0,0 +1,42 @@
+import java.util.TreeSet;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Aditya", 95);
+        Student s2 = new Student("Aditya", 95);
+
+        TreeSet<Student> set = new TreeSet<>();
+
+        set.add(s1);
+        set.add(s2);
+
+        System.out.println(set.size());
+    }
+    
+}
+
+
+
+class Student implements Comparable<Student>{
+    int marks;
+    String name;
+
+    Student(String name,int marks){
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public int compareTo(Student other){
+        // return this.marks - other.marks;
+        // return  other.marks- this.marks ;
+
+        if(this.marks != other.marks){
+            return this.marks - other.marks;
+        }
+
+        return this.name.compareTo(other.name);
+
+
+    }
+}
